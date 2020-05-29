729d66bdab958051684fe1b789174cdfafcc6a17
Christian Harris <cp_harris1011@hotmail.com>
2020-05-29 06:40:11 -0400

    define the piece colors for each level

--- src/constants.js
+++ src/constants.js
@@ -7,6 +7,34 @@ export const RED_COLOR = "red";
 export const BLUE_COLOR = "#2105f2";
 export const WHITE_COLOR = "white";
 
+// color 1 is WHITE_COLOR, which is the main color of I, T, and O
+// color 2 is the main color of L and Z
+export const COLOR_2 = {
+  0: "rgb(60,188,252)",
+  1: "rgb(148,248,24)",
+  2: "rgb(248,120,248)",
+  3: "rgb(88,216,84)",
+  4: "rgb(88,248,152)",
+  5: "rgb(104,136,252)",
+  6: "rgb(124,124,124)",
+  7: "rgb(168,0,32)",
+  8: "rgb(248,56,0)",
+  9: "rgb(252,160,68)",
+}
+// COLOR_3 is the main color of J and S
+export const COLOR_3 = {
+  0: "rgb(0,88,248)",
+  1: "rgb(0,168,0)",
+  2: "rgb(216,0,204)",
+  3: "rgb(0,88,248)",
+  4: "rgb(228,0,88",
+  5: "rgb(88,248,152)",
+  6: "rgb(248,56,0)",
+  7: "rgb(104,68,252)",
+  8: "rgb(0,88,248)",
+  9: "rgb(248,56,0)",
+}
+
 export const Direction = Object.freeze({
   LEFT: 1,
   RIGHT: 2,
@@ -43,6 +71,7 @@ export const GRAVITY = {
   17: 3,
   18: 3,
   19: 2,
+  29: 1
 };
 
 export const GameState = {
