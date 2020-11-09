e7c629ba6bd46812e65fd12ea656188f2daa698b
Greg Cannon <gregoryc@outlook.com>
2020-11-09 04:01:29 -0800

    Fix webpack config so the compiled JS goes to the right place

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+const path = require("path");
+
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
+    path: path.resolve(__dirname, "docs"),
     publicPath: "docs",
   },
 };
