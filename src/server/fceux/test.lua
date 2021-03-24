86120e904a279ef4211bfaf94707705471506edb
Greg Cannon <gregoryc@outlook.com>
2021-03-24 06:19:17 -0700

    clean up lua test file

--- src/server/fceux/test.lua
+++ src/server/fceux/test.lua
@@ -1,29 +1,36 @@
-local socket = require("socket")
-local http = require("socket.http")
+local os = require("os")
 
--- local r, c, h = http.request{ 
---     url = "http://localhost:3000/uu0,0,0,0,0,0,0,0,0,0v,u0,0,0,0,0,0,0,0,0,0v,u0,0,0,0,0,0,0,0,0,0v,u0,0,0,0,0,0,0,0,0,0v,u0,0,0,0,0,0,0,0,0,0v,u0,0,0,0,0,0,0,0,0,0v,u0,0,0,0,0,0,0,0,0,0v,u0,0,0,0,0,0,0,0,0,0v,u0,0,0,0,0,0,0,0,0,0v,u0,0,0,0,0,0,0,0,0,0v,u0,0,0,0,0,0,0,0,0,0v,u0,0,0,0,0,0,0,0,0,0v,u0,0,0,0,0,0,0,0,0,0v,u0,0,0,0,0,0,0,0,0,0v,u0,0,0,0,0,0,0,0,0,0v,u0,0,0,0,0,0,0,0,0,0v,u0,0,0,0,0,0,0,0,0,0v,u0,0,0,0,0,0,0,0,0,0v,u0,1,0,0,0,0,0,0,0,0v,u1,1,1,0,0,0,0,0,0,0vv/T/J/18/0", 
--- }
-
--- print(r)
--- print(c)
--- print(h)
-
-local data = ""
-
-local function collect(chunk)
-  if chunk ~= nil then
-    data = data .. chunk
-  end
-  return true
+function testBackgroundProcess()
+  -- Async
+  result = io.popen("bgThread")
+  
+  -- Synchronous
+  -- result = os.execute("bgThread")
+  
+  print(result)
+  print("Done!")
 end
 
-local ok, statusCode, headers, statusText = http.request {
-  method = "GET",
-  url = "http://localhost:3000/00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001110/T/J/18/0",
-  sink = collect
-}
+-- testBackgroundProcess()
 
-print(data)
+function testApiCall()
+  local socket = require("socket")
+  local http = require("socket.http")
 
--- print("I think it worked :D")
+  local data = ""
+
+  local function collect(chunk)
+    if chunk ~= nil then
+      data = data .. chunk
+    end
+    return true
+  end
+
+  local ok, statusCode, headers, statusText = http.request {
+    method = "GET",
+    url = "http://localhost:3000/00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001110/T/J/18/0",
+    sink = collect
+  }
+
+  print(data)
+end
