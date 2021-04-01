fc4e7bb8ec093659b51168adf0346a75a89fd919
Greg Cannon <gregoryc@outlook.com>
2021-03-31 22:39:38 -0700

    Refactor out AI mode management to its own file

--- src/server/evaluator.js
+++ src/server/evaluator.js
@@ -7,67 +7,6 @@ const SquareState = utils.SquareState;
 const NUM_ROW = utils.NUM_ROW;
 const NUM_COLUMN = utils.NUM_COLUMN;
 
-function getAiMode(board, lines, level, aiParams) {
-  if (level >= 29) {
-    return AI_MODE.KILLSCREEN;
-  }
-  if (lines >= 220) {
-    return AI_MODE.NEAR_KILLSCREEN;
-  }
-  if (shouldUseDigMode(board, aiParams)) {
-    return AI_MODE.DIG;
-  }
-  return AI_MODE.STANDARD;
-}
-
-/** The logic here is quite complex, it's a shame it has to be manually coded instead of trained.
- * Current logic:
- *  - If you have a high-up hole that is blocking the well, go dig it out immediately
- *  - If the hole is in the tetris zone, go dig it
- *  - If the hole doesn't have many filled lines above it, go dig it
- *  - Otherwise, play on and plan to play a few rows off the bottom
- */
-function shouldUseDigMode(board, aiParams) {
-  // Calculate where the next Tetris will be built
-  let row = 0;
-  while (row < NUM_ROW && board[row][9] == SquareState.EMPTY) {
-    row++;
-  }
-  const tetrisZoneStart = row - 4;
-  const tetrisZoneEnd = row - 1;
-
-  function holeWarrantsDigging(row, firstFullRow) {
-    const blockingWell = board[row][NUM_COLUMN - 1] === SquareState.FULL;
-    return (
-      (blockingWell && NUM_ROW - row > aiParams.MAX_DIRTY_TETRIS_HEIGHT) ||
-      (row >= tetrisZoneStart && row < tetrisZoneEnd) ||
-      row - firstFullRow < 4
-    );
-  }
-
-  // Check for holes that are either in the Tetris zone or have <= 3 full lines above them (are reasonably accessible with burns)
-  for (let col = 0; col < NUM_COLUMN - 1; col++) {
-    // Navigate past the empty space above each column
-    let row = 0;
-    while (row < NUM_ROW && board[row][col] === SquareState.EMPTY) {
-      row++;
-    }
-    const firstFullRow = row;
-
-    // Now that we're in the stack, if there are empty cells, they're holes
-    while (row < NUM_ROW - 1) {
-      row++;
-      if (board[row][col] === SquareState.EMPTY) {
-        // Found hole
-        if (holeWarrantsDigging(row, firstFullRow)) {
-          return true;
-        }
-      }
-    }
-  }
-  return false;
-}
-
 /** If there is a spire in the middle of the board, return its height above the scare line. Otherwise, return 0. */
 function getSpireHeight(surfaceArray, scareHeight) {
   let spireHeight = 0;
@@ -458,6 +397,5 @@ function pickBestMoveNoNextBox(
 module.exports = {
   pickBestNMoves,
   pickBestMoveNoNextBox,
-  getLineClearValue,
-  getAiMode,
+  getLineClearValue
 };
