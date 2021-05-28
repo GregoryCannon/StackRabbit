d602504236f147dcf7c8100b49a67ed16e059c2a
Greg Cannon <gregoryc@outlook.com>
2021-05-28 08:46:49 -0700

    Fix UI build after package rename

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   target: "node",
   mode: "development",
-  entry: "./src/webclient/index.js",
+  entry: "./src/web_client/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "docs"),
