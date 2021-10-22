63ac5ba047fe781d3bc4b448a13b18f1c8d0525b
Greg Cannon <gregoryc@outlook.com>
2021-10-22 08:13:51 -0700

    Fix compilation errors relating to the ranks file, so it compiles from fresh install

--- src/cpp_modules/data/ranks_output.hpp
+++ src/cpp_modules/data/ranks_output.hpp
@@ -1,7 +1,7 @@
 #ifndef RANKS
 #define RANKS
 
-// extern const short surfaceRanksRaw[43046721] = {};
-extern const short surfaceRanksRaw[43046721];
+extern const short surfaceRanksRaw[43046721] = {};
+// extern const short surfaceRanksRaw[43046721];
 
 #endif
