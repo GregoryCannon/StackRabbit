ebebe1c6dd0179b96aa142c55e758f21838e4869
Greg Cannon <gregoryc@outlook.com>
2023-11-26 23:58:42 -0800

    Remove one more exception

--- src/cpp_modules/src/main.cpp
+++ src/cpp_modules/src/main.cpp
@@ -23,7 +23,10 @@ template<typename ... Args>
 std::string string_format( const std::string& format, Args ... args )
 {
     int size_s = std::snprintf( nullptr, 0, format.c_str(), args ... ) + 1; // Extra space for '\0'
-    if( size_s <= 0 ){ throw std::runtime_error( "Error during formatting." ); }
+    if( size_s <= 0 ){
+//      throw std::runtime_error( "Error during formatting." );
+      return NULL;
+    }
     auto size = static_cast<size_t>( size_s );
     std::unique_ptr<char[]> buf( new char[ size ] );
     std::snprintf( buf.get(), size, format.c_str(), args ... );
