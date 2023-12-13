GregoryCannon/StackRabbit

commit 53ee8def6b98eea862e8a4792500663c6fbc7646

Fix JS move search

src/server/move_search.ts
@@ -454,6 +454,11 @@ function repeatedlyShiftPiece(
       }
     }
 
+    // If we just shifted and are in the intended rotation, then this is a legal placement
+    if (addNewPlacement) {
+      legalPlacementSimStates.push({ ...simState });
+    }
+
     if (isGravityFrame) {
       if (
         pieceCollision(
@@ -481,11 +486,6 @@ function repeatedlyShiftPiece(
 
     simState.frameIndex += 1;
     simState.arrFrameIndex += 1;
-
-    // If we just shifted and are in the intended rotation, then this is a legal placement
-    if (addNewPlacement) {
-      legalPlacementSimStates.push({ ...simState });
-    }
   }
 }
 

src/server/move_search_test.ts
@@ -201,6 +201,16 @@ function tapRangeTest() {
   ) {
     console.log(`Failed: 9 high 3 tap 29 13.5 Hz. Expected: ${expected16}`);
   }
+
+  const expected17 = true;
+  if (
+    canDoPlacement(getTestBoardWithHeight(2), 29, "I", 1, -5, "....X..X..X..X..X") !==
+    expected17
+  ) {
+    console.log(`Failed: 2 high 5 tap 29 15 Hz. Expected: ${expected17}`);
+  }
+
+  console.log("Run completed.");
 }
 
 function lastMinuteRotationsTest() {
@@ -360,7 +370,8 @@ function generateTestCases() {
 }
 
 // generateTestCases();
-testNumLegalAdjustments();
+// testNumLegalAdjustments();
+tapRangeTest();
 // for (const poss of testSingleCase([-5, 10, 3, 41])) {
 //   console.log(poss.placement);
 // }

commit b07eb58e58e3bd91b57c9e8093fad8ce7f1046e0

Fix move search bugs introduced by commit 53ee8de

src/server/move_search.ts
@@ -163,6 +163,36 @@ function exploreLegalPlacementsUntilLock(
     let highestRegisteredY = -1; // Tracks the Y values already registered to avoid duplicates
 
     while (true) {
+      const registerPossibility = () => {
+        // Piece would lock in!
+        // Do some housekeeping and then generate the possibility
+        lockHeightLookup.set(
+          simState.rotationIndex + "," + simState.x,
+          simState.y
+        );
+        lockPossibilities.push(
+          getPossibilityFromSimState(
+            simState,
+            simParams,
+            inputSequenceWithWait,
+            /* inputCost= */ 0
+          )
+        );
+      };
+
+      // If it collides immediately, quit
+      if (
+        pieceCollision(
+          simParams.board,
+          simState.x,
+          simState.y,
+          currentRotationPiece
+        )
+      ) {
+        registerPossibility();
+        break;
+      }
+
       // Run simulated frames of only gravity
       const isGravityFrame =
         simState.frameIndex % simParams.gravity === simParams.gravity - 1; // Returns true every Nth frame, where N = gravity
@@ -419,6 +449,7 @@ function repeatedlyShiftPiece(
     );
     const isGravityFrame = simState.frameIndex % gravity === gravity - 1; // Returns true every Nth frame, where N = gravity
     let addNewPlacement = false;
+    let lockAfterThisFrame = false;
 
     if (isInputFrame) {
       // Try the shift input, then the rotation input
@@ -454,11 +485,6 @@ function repeatedlyShiftPiece(
       }
     }
 
-    // If we just shifted and are in the intended rotation, then this is a legal placement
-    if (addNewPlacement) {
-      legalPlacementSimStates.push({ ...simState });
-    }
-
     if (isGravityFrame) {
       if (
         pieceCollision(
@@ -468,8 +494,8 @@ function repeatedlyShiftPiece(
           rotationsList[simState.rotationIndex]
         )
       ) {
-        debugLog(simState, simParams, "GRAVITY");
-        return rangeCurrent; // Piece would lock in
+        console.log("DEBUG GRAVITY");
+        lockAfterThisFrame = true;
       }
       simState.y++;
     }
@@ -486,6 +512,16 @@ function repeatedlyShiftPiece(
 
     simState.frameIndex += 1;
     simState.arrFrameIndex += 1;
+
+    // If we previously marked that we reached a legal placement this frame, save this as a legal placement.
+    // We do this at the very end such that the whole frame is done processing, and simState is ready for the next frame (e.g. looking for tucks)
+    if (addNewPlacement) {
+      legalPlacementSimStates.push({ ...simState });
+    }
+
+    if (lockAfterThisFrame) {
+      return rangeCurrent; // Piece would lock in, no further search needed
+    }
   }
 }
 
@@ -711,7 +747,7 @@ function performSimulationRotation(
 }
 
 function debugLog(simState: SimState, simParams: SimParams, reason: string) {
-  // console.log("DEBUG: " + reason);
+  // console.log("\nDEBUG: " + reason);
   // logBoard(
   //   getBoardAndLinesClearedAfterPlacement(
   //     simParams.board,

src/server/params.ts
@@ -30,7 +30,7 @@ export const SHOULD_PUSHDOWN = false;
 export const DISABLE_LOGGING = true;
 
 // Calculated automatically
-export const USE_RANKS = true
+export const USE_RANKS = true;
 export const IS_NON_RIGHT_WELL = WELL_COLUMN !== 9;
 export const KILLSCREEN_LINES = IS_PAL ? 130 : 230;
 export const KILLSCREEN_LEVEL = IS_PAL ? 19 : 29;