c1321a4487d543c9dd950371fba7ba87f0165961
Timothee Groleau <timothee.groleau@gmail.com>
2024-10-08 22:24:25 +0800

    chore: ensure there is no name collision for the exposed js functions

--- src/cpp_modules/src/wasm.cpp
+++ src/cpp_modules/src/wasm.cpp
@@ -4,37 +4,37 @@
 #undef NONE
 #include <emscripten/bind.h>
 
-std::string getLockValueLookup(std::string inputStr) {
+std::string jsGetLockValueLookup(std::string inputStr) {
     const char* cInputStr = inputStr.c_str();
     return mainProcess(cInputStr, GET_LOCK_VALUE_LOOKUP);
 }
 
-std::string getMove(std::string inputStr) {
+std::string jsGetMove(std::string inputStr) {
     const char* cInputStr = inputStr.c_str();
     return mainProcess(cInputStr, GET_MOVE);
 }
 
-std::string getTopMoves(std::string inputStr) {
+std::string jsGetTopMoves(std::string inputStr) {
     const char* cInputStr = inputStr.c_str();
     return mainProcess(cInputStr, GET_TOP_MOVES);
 }
 
-std::string getTopMovesHybrid(std::string inputStr) {
+std::string jsGetTopMovesHybrid(std::string inputStr) {
     const char* cInputStr = inputStr.c_str();
     return mainProcess(cInputStr, GET_TOP_MOVES_HYBRID);
 }
 
-// std::string rateMove(std::string inputStr) {
-//     const char* cInputStr = inputStr.c_str();
-//     return mainProcess(cInputStr, RATE_MOVE);
-// }
+std::string jsRateMove(std::string inputStr) {
+    const char* cInputStr = inputStr.c_str();
+    return mainProcess(cInputStr, RATE_MOVE);
+}
 
 
 EMSCRIPTEN_BINDINGS(my_module) {
-    emscripten::function("getLockValueLookup", &getLockValueLookup);
-    emscripten::function("getMove", &getMove);
-    emscripten::function("getTopMoves", &getTopMoves);
-    emscripten::function("getTopMovesHybrid", &getTopMovesHybrid);
-    // emscripten::function("rateMove", &rateMove);
+    emscripten::function("getLockValueLookup", &jsGetLockValueLookup);
+    emscripten::function("getMove", &jsGetMove);
+    emscripten::function("getTopMoves", &jsGetTopMoves);
+    emscripten::function("getTopMovesHybrid", &jsGetTopMovesHybrid);
+    emscripten::function("rateMove", &jsRateMove);
 }
 
