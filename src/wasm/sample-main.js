d2de74bb5aa3cf6ae0145d02ba1bb8e8433c3e2c
Timothee Groleau <timothee.groleau@gmail.com>
2024-10-09 22:17:45 +0800

    feat: add demo calls to all 5 methods in sample app

--- src/wasm/sample-main.js
+++ src/wasm/sample-main.js
@@ -17,21 +17,52 @@ stackRabbitWorker.rpc = (...command) => {
 };
 
 stackRabbitWorker.onmessage = async function (event) {
-    if (event.data.type == 'init') {
-        const params = {
-            level: 18,
-            lines: 2,
-            inputFrameTimeline: 'X....',
-            currentPiece: 'J',
-            nextPiece: 'L',
-            board: "00000000000000000000000000000000000000000000000000000000000000000011100000001110000000111100000111110000011110000011111100011101110011101110001111111000111111100111111110011111111001111111101111111110",
-        };
-        
-        const start = Date.now();
-        const move = await stackRabbitWorker.rpc('getMove', params);
-        const elapsed = Date.now() - start;
-    
-        output.textContent = `${JSON.stringify(move)}\nin ${elapsed} milliseconds`;
-    }
+    if (event.data.type == 'init') init();
 };
+
+
+const params = {
+    level: 18,
+    lines: 50,
+    inputFrameTimeline: 'X....',
+    currentPiece: 'I',
+    nextPiece: 'I',
+    board: "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000011000000001100000000110000001011110111101111111110111111111011111111101111111110",
+};
+
+const extraParams = {
+    rateMove: {
+        secondBoard: "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000011001000001100100000110010001011111111101111111110111111111011111111101111111110",
+    }
+}
+
+async function runFunction(funcName) {
+    const args = {
+        ...params,
+        ...extraParams[funcName]
+    };
+
+    start = Date.now();
+    res = await stackRabbitWorker.rpc(funcName, args);
+    elapsed = Date.now() - start;
+
+    output.textContent = `${funcName} - ${elapsed} milliseconds\n\n`;
+    output.textContent += JSON.stringify(res, null, 2);
+}
+
+function init() {
+    [
+        'getLockValueLookup',
+        'getMove',
+        'getTopMoves',
+        'getTopMovesHybrid',
+        'rateMove'
+    ]
+    .forEach(funcName => {
+        const button = document.createElement('button');
+        button.textContent = funcName;
+        button.addEventListener('click', () => runFunction(funcName));
+        document.getElementById('controls').append(button);
+    });
+}
 
--- src/wasm/wasmRabbit-worker.js
+++ src/wasm/wasmRabbit-worker.js
@@ -17,6 +17,8 @@ const SR_PIECES_INDEXES = {
 	Z: 6,
 };
 
+const DELIM = '|';
+
 function getStackRabbitArgString(args) {
 	const {
 		level,
@@ -36,16 +38,32 @@ function getStackRabbitArgString(args) {
 		inputFrameTimeline,
 		'', // playoutCount?
 		// '', // playoutLength?
-	].join('|');
+	].join(DELIM);
 }
 
 // supported methods
 const API = {
+	getLockValueLookup: args => {
+		const rawRes = Module.getLockValueLookup(getStackRabbitArgString(args));
+		return JSON.parse(rawRes);
+	},
 	getMove: args => {
-		const start = Date.now();
 		const rawRes = Module.getMove(getStackRabbitArgString(args));
-		const getMoveTime = Date.now() - start;
-		console.log({ getMoveTime });
+		return JSON.parse(rawRes);
+	},
+	getTopMoves: args => {
+		const rawRes = Module.getTopMoves(getStackRabbitArgString(args));
+		return JSON.parse(rawRes);
+	},
+	getTopMovesHybrid: args => {
+		const rawRes = Module.getTopMovesHybrid(getStackRabbitArgString(args));
+		return JSON.parse(rawRes);
+	},
+	rateMove: args => {
+		// dirty trick to account for second board
+		args.board += DELIM + args.secondBoard;
+
+		const rawRes = Module.rateMove(getStackRabbitArgString(args));
 		return JSON.parse(rawRes);
 	},
 };
